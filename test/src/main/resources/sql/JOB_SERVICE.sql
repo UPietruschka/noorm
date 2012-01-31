50eee36a5c39f57904338e0a7245565789fb96e0
unknown <pietrusu@.accounts.intern>
2012-01-31 21:46:08 +0100

    New test service added to test for stored procedures returning scalar values and stored procedures with DML operations.

---
+++ test/src/main/resources/sql/JOB_SERVICE.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE
+PACKAGE job_service AS
+
+  PROCEDURE increase_salary(p_department_id IN NUMBER,
+                            p_percentage IN FLOAT);
+
+  PROCEDURE get_employee_count(p_employee_count OUT NUMBER);
+
+END job_service;
+/
+
+CREATE OR REPLACE
+PACKAGE BODY job_service AS
+
+  PROCEDURE increase_salary(p_department_id IN NUMBER,
+                            p_percentage IN FLOAT) AS
+  BEGIN
+    UPDATE employees
+    SET    salary = salary * (1 + p_percentage)
+    WHERE  department_id = p_department_id;
+  END increase_salary;
+
+  PROCEDURE get_employee_count(p_employee_count OUT NUMBER) AS
+  BEGIN
+    SELECT COUNT(*) INTO p_employee_count FROM employees;
+  END get_employee_count;
+
+END job_service;
+/
---
+++ test/src/test/java/org/noorm/test/JobServiceTest.java
@@ -0,0 +1,48 @@
+package org.noorm.test;
+
+import org.junit.Test;
+import org.noorm.jdbc.DataSourceProvider;
+import org.noorm.test.hr.beans.EmployeesBean;
+import org.noorm.test.hr.services.EmployeeSearch;
+import org.noorm.test.hr.services.JobService;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * @author Ulf Pietruschka / ulf.pietruschka@etenso.com
+ *         Date: 31.01.12
+ *         Time: 18:25
+ */
+public class JobServiceTest {
+
+	private static final Long EMPLOYEE_COUNT = 107L;
+
+	@Test
+	public void testGetEmployeeCount() {
+		
+		JobService jobService = JobService.getInstance();
+		final Long employeeCount = jobService.getEmployeeCount();
+		assertEquals(EMPLOYEE_COUNT, employeeCount);
+	}
+
+	@Test
+	public void testIncreaseSalary() {
+
+		DataSourceProvider.begin();
+		JobService jobService = JobService.getInstance();
+		// Increase the salary for the given department by 8%
+		jobService.increaseSalary(90L, 0.08d);
+		EmployeeSearch employeeSearch = EmployeeSearch.getInstance();
+		// Search for an exact match of the first employee with increased salary
+		List<EmployeesBean> employeesBeanList0 =
+				employeeSearch.findEmployeesByFilter(null, null, null, null, 25920L, 25920L);
+		assertEquals(1, employeesBeanList0.size());
+		// Search for an exact match of the second and third employee with increased salary
+		List<EmployeesBean> employeesBeanList1 =
+				employeeSearch.findEmployeesByFilter(null, null, null, null, 18360L, 18360L);
+		assertEquals(2, employeesBeanList1.size());
+		DataSourceProvider.rollback();
+	}
+}
