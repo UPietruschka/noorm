e087f15a945ff1e9559deba209769360168be16e
UPietruschka <ulf.pietruschka@etenso.com>
2012-11-02 16:12:24 +0100

    Character columns changed to NVARCHAR2 to include this Oracle type into testing.

--- test/basic/src/main/resources/sql/OPT_LOCK_TABLES.sql
+++ test/basic/src/main/resources/sql/OPT_LOCK_TABLES.sql
@@ -2,7 +2,7 @@ DROP TABLE opt_lock_long;
 CREATE TABLE opt_lock_long
 (
  id NUMBER  NOT NULL,
- text VARCHAR2(32),
+ text NVARCHAR2(32),
  version NUMBER DEFAULT 1
 );
 ALTER TABLE opt_lock_long ADD CONSTRAINT opt_lock_long_pk PRIMARY KEY (id);
@@ -14,7 +14,7 @@ DROP TABLE opt_lock_timestamp;
 CREATE TABLE opt_lock_timestamp
 (
  id NUMBER  NOT NULL,
- text VARCHAR2(32),
+ text NVARCHAR2(32),
  version TIMESTAMP DEFAULT SYSTIMESTAMP
 );
 ALTER TABLE opt_lock_timestamp ADD CONSTRAINT opt_lock_timestamp_pk PRIMARY KEY (id);
