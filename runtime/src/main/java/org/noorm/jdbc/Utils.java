64e8c76877c2671d4bc716e9a2a0278d75716f9d
UPietruschka <ulf.pietruschka@etenso.com>
2012-11-06 19:24:47 +0100

    Documentation improved. Regex replacement improved (replaceFirst is used instead of replaceAll; the latter can produce unwanted side-effects).  Enum display name normalization improved (leading digits are not supported; an underscore is prepended in this case).

--- runtime/src/main/java/org/noorm/jdbc/Utils.java
+++ runtime/src/main/java/org/noorm/jdbc/Utils.java
@@ -21,6 +21,12 @@ public class Utils {
 	private static final String ENUM_UNSUPPORTED_REGEX = "[ /\\-\\,\\.;]";
 	private static final String ENUM_UNSUPPORTED_REGEX_SUBSTITUTE = "_";
 
+    /**
+     * Converts a Java name into a database name. Inversion of convertDBName2JavaName.
+     * @see #convertDBName2JavaName(String, boolean)
+     * @param pJavaName the Java name
+     * @return the database name
+     */
 	public static String convertJavaName2DBName(final String pJavaName) {
 
 		char[] javaNameChars = pJavaName.toCharArray();
@@ -41,6 +47,7 @@ public class Utils {
      * to separate parts of the name by underscores, e.g. "FIRST_NAME", "ORDER_NUMBER", etc. In Java, a widely
      * used convention is to separate parts of the name by capitalizing the first name of the new part, e.g.
      * "firstName", "orderNumber".
+     *
      * @param pDBName a database object name
      * @param pCapitalizeFirst indicator, whether the first letter in the converted name is to be capitalized
      * @return the converted name
@@ -63,18 +70,42 @@ public class Utils {
 		return javaName.toString();
 	}
 
+    /**
+     * Converts an Oracle table name to a short table name.
+     * @see #convertTableName2JavaName(String, java.util.List)
+     * @param pTableName the database table name
+     * @param pIgnoreTableNamePrefixes optional list of table name prefixes to be ignored at conversion
+     * @return the short table name
+     */
 	public static String convertTableName2ShortName(final String pTableName,
 												    final List<String> pIgnoreTableNamePrefixes) {
 
 		return convertTableName2JavaName(pTableName, pIgnoreTableNamePrefixes);
 	}
 
-	public static String convertTableName2BeanName(final String pTableName,
+    /**
+     * Converts an Oracle table name to a Java Bean name.
+     * @see #convertTableName2JavaName(String, java.util.List)
+     * @param pTableName the database table name
+     * @param pIgnoreTableNamePrefixes optional list of table name prefixes to be ignored at conversion
+     * @return the Java Bean name
+     */
+    public static String convertTableName2BeanName(final String pTableName,
 												   final List<String> pIgnoreTableNamePrefixes) {
 
 		return convertTableName2JavaName(pTableName, pIgnoreTableNamePrefixes).concat(BEAN_NAME_APPENDIX);
 	}
 
+    /**
+     * Converts an Oracle table name to a Java name. The optional list of ignored table name prefixes is used
+     * to remove the table name prefix from the generated name. This is useful, when many or all tables in a
+     * database schema share a common name prefix, which should be visible in the generated code (e.g. "TBL_").
+     *
+     * @see #convertDBName2JavaName(String, boolean)
+     * @param pTableName the database table name
+     * @param pIgnoreTableNamePrefixes optional list of table name prefixes to be ignored at conversion
+     * @return the Java name
+     */
 	public static String convertTableName2JavaName(final String pTableName,
 												   final List<String> pIgnoreTableNamePrefixes) {
 
@@ -86,9 +117,18 @@ public class Utils {
 				}
 			}
 		}
-		return Utils.convertDBName2JavaName(nameBaseTableName, true);
+		return convertDBName2JavaName(nameBaseTableName, true);
 	}
 
+    /**
+     * Maps the given Oracle type to the corresponding Java type, which represents the Oracle type in
+     * the generated Java Bean class (e.g. "VARCHAR2" -> "String").
+     *
+     * @param pOracleType the Oracle type name
+     * @param pDataPrecision the data precision of the Oracle type, if available
+     * @param pDataScale the data scale of the Oracle type, if available
+     * @return the Java type name
+     */
 	public static String convertOracleType2JavaType(final String pOracleType,
 													final Long pDataPrecision,
 													final Long pDataScale) {
@@ -127,16 +167,30 @@ public class Utils {
 		return javaType;
 	}
 
-	public static String getPropertyString(final String pRegex, final Properties pProperties) {
+    /**
+     * Replaces strings or parts of strings with a replacement expression.
+     * This method iterates through the given properties, which should contain pairs of search pattern and
+     * replacement patterns for string replacement in the input string. For the first search pattern, which
+     * matches the input string, the replacement pattern is applied. The simplest form of string replacement
+     * is a direct replacement of the search pattern with the replacement pattern without any usage of
+     * regular expressions (e.g. "CUSTOMER" -> "CUSTOMER_SEQ"). Regular expressions allow for a compact
+     * representation of more generic replacements (e.g. search/replace expressions "(.*)" / "$1_SEQ" also
+     * results in the replacement string "CUSTOMER_SEQ" for input string "CUSTOMER").
+     *
+     * @param pInput the input string subject to regular expression replacement
+     * @param pProperties a collection of key/value pairs for search/replace expressions
+     * @return the modified input string
+     */
+	public static String getPropertyString(final String pInput, final Properties pProperties) {
 
 		String propertyString = "";
         if (pProperties != null) {
             for (String searchPattern : pProperties.stringPropertyNames()) {
                 final Pattern finder = Pattern.compile(searchPattern);
-                final Matcher matcher = finder.matcher(pRegex);
+                final Matcher matcher = finder.matcher(pInput);
                 if (matcher.matches()) {
                     final String replacePattern = pProperties.getProperty(searchPattern);
-                    propertyString = matcher.replaceAll(replacePattern);
+                    propertyString = matcher.replaceFirst(replacePattern);
                     break;
                 }
             }
@@ -144,9 +198,22 @@ public class Utils {
 		return propertyString;
 	}
 
-	public static String getNormalizedDisplayColumnValue(final String pDisplayColumnValue) {
+    /**
+     * Converts the given input into a valid format for the Java enum display name.
+     * Java enum display names are not allowed to contain the characters " ", "/", "-", ",", ".", ";".
+     * These characters are replaced by an underscore. In addition, when the name starts with a digit,
+     * an underscore is placed at the beginning of the normalized name.
+     *
+     * @param pDisplayNameValue the name to be normalized
+     * @return the normalized name
+     */
+	public static String getNormalizedDisplayColumnValue(final String pDisplayNameValue) {
 
-		return pDisplayColumnValue.trim().toUpperCase().replaceAll
-				(ENUM_UNSUPPORTED_REGEX, ENUM_UNSUPPORTED_REGEX_SUBSTITUTE);
+        String prefix = "";
+        if (pDisplayNameValue.matches("[0-9].*")) {
+            prefix = ENUM_UNSUPPORTED_REGEX_SUBSTITUTE;
+        }
+		return prefix.concat(pDisplayNameValue.trim().toUpperCase().replaceAll
+				(ENUM_UNSUPPORTED_REGEX, ENUM_UNSUPPORTED_REGEX_SUBSTITUTE));
 	}
 }
