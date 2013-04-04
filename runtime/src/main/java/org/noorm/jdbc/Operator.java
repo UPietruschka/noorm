b63ba4decf78cbdd71eabb3dc7f8d12602f45304
UPietruschka <ulf.pietruschka@etenso.com>
2013-04-04 17:21:58 +0200

    Configuration API (internal) improved. Test cases modified to work with Maven 2.2.1, too.

--- runtime/src/main/java/org/noorm/jdbc/Operator.java
+++ runtime/src/main/java/org/noorm/jdbc/Operator.java
@@ -2,9 +2,12 @@ package org.noorm.jdbc;
 
 /**
  * Operator definitions for the generic SQL query construction.
- * Supported are the operators equal-to (default, when used within QueryColumn), not-equal-to, greater-than,
- * greater-than-or-equal-to, less-than and less-than-or-equal-to. The exact syntax as listed is to be used when
- * configuring SQL queries using query declarations in the Maven generator plugin.
+ * Supported are the operators EQUAL_TO (default, when used within QueryColumn), NOT_EQUAL_TO, GREATER_THAN,
+ * GREATER_THAN_OR_EQUAL_TO, LESS_THAN, LESS_THAN_OR_EQUAL_TO, LIKE, IS_NULL and IS_NOT_NULL. The exact syntax as
+ * listed is to be used when configuring SQL queries using query declarations in the Maven generator plugin.
+ *
+ * The setter for the operator name with the String parameter is used by Maven 2.2.1 (and earlier versions) only.
+ * Maven 3.X is able to resolve enums and is thus able to the use the setter with the enum instead.
  *
  * @author Ulf Pietruschka / ulf.pietruschka@etenso.com
  *         Date: 20.01.13
@@ -12,39 +15,47 @@ package org.noorm.jdbc;
  */
 public class Operator {
 
-    private OperatorName operatorName;
+    private Name operatorName;
     private String operatorSyntax;
     private boolean unary = false;
 
     public Operator() {
-        operatorName = OperatorName.EQUAL_TO;
+        operatorName = Name.EQUAL_TO;
     }
 
     public Operator(final String pOperatorName) {
         setOperatorName(pOperatorName);
     }
 
-    public OperatorName getOperatorName() {
+    public Operator(final Name pOperatorName) {
+        setOperatorName(pOperatorName);
+    }
+
+    public Name getOperatorName() {
         return operatorName;
     }
 
     public void setOperatorName(final String pOperatorName) {
 
-        String normalizedName = pOperatorName.toUpperCase();
-        normalizedName = normalizedName.replace('-', '_');
-        operatorName = OperatorName.valueOf(normalizedName);
-        if (operatorName.equals(OperatorName.EQUAL_TO)) { operatorSyntax = " = "; }
-        if (operatorName.equals(OperatorName.NOT_EQUAL_TO)) { operatorSyntax = " != "; }
-        if (operatorName.equals(OperatorName.GREATER_THAN)) { operatorSyntax = " > "; }
-        if (operatorName.equals(OperatorName.GREATER_THAN_OR_EQUAL_TO)) { operatorSyntax = " >= "; }
-        if (operatorName.equals(OperatorName.LESS_THAN)) { operatorSyntax = " < "; }
-        if (operatorName.equals(OperatorName.LESS_THAN_OR_EQUAL_TO)) { operatorSyntax = " <= "; }
-        if (operatorName.equals(OperatorName.LIKE)) { operatorSyntax = " LIKE "; }
-        if (operatorName.equals(OperatorName.IS_NULL)) {
+        operatorName = Name.valueOf(pOperatorName);
+        setOperatorName(operatorName);
+    }
+
+    public void setOperatorName(final Name pOperatorName) {
+
+        operatorName = pOperatorName;
+        if (operatorName.equals(Name.EQUAL_TO)) { operatorSyntax = " = "; }
+        if (operatorName.equals(Name.NOT_EQUAL_TO)) { operatorSyntax = " != "; }
+        if (operatorName.equals(Name.GREATER_THAN)) { operatorSyntax = " > "; }
+        if (operatorName.equals(Name.GREATER_THAN_OR_EQUAL_TO)) { operatorSyntax = " >= "; }
+        if (operatorName.equals(Name.LESS_THAN)) { operatorSyntax = " < "; }
+        if (operatorName.equals(Name.LESS_THAN_OR_EQUAL_TO)) { operatorSyntax = " <= "; }
+        if (operatorName.equals(Name.LIKE)) { operatorSyntax = " LIKE "; }
+        if (operatorName.equals(Name.IS_NULL)) {
             operatorSyntax = " IS NULL ";
             unary = true;
         }
-        if (operatorName.equals(OperatorName.IS_NOT_NULL)) {
+        if (operatorName.equals(Name.IS_NOT_NULL)) {
             operatorSyntax = " IS NOT NULL ";
             unary = true;
         }
@@ -62,19 +73,19 @@ public class Operator {
         unary = pUnary;
     }
 
-    enum OperatorName {
+    public enum Name {
 
-        EQUAL_TO("equal-to"),
-        NOT_EQUAL_TO("not-equal-to"),
-        GREATER_THAN("greater-than"),
-        GREATER_THAN_OR_EQUAL_TO("greater-than-or-equal-to"),
-        LESS_THAN("less-than"),
-        LESS_THAN_OR_EQUAL_TO("less-than-or-equal-to"),
-        LIKE("like"),
-        IS_NULL("is-null"),
-        IS_NOT_NULL("is-not-null");
+        EQUAL_TO("EQUAL_TO"),
+        NOT_EQUAL_TO("NOT_EQUAL_TO"),
+        GREATER_THAN("GREATER_THAN"),
+        GREATER_THAN_OR_EQUAL_TO("GREATER_THAN_OR_EQUAL_TO"),
+        LESS_THAN("LESS_THAN"),
+        LESS_THAN_OR_EQUAL_TO("LESS_THAN_OR_EQUAL_TO"),
+        LIKE("LIKE"),
+        IS_NULL("IS_NULL"),
+        IS_NOT_NULL("IS_NOT_NULL");
 
-        private OperatorName(final String pOperatorName) {
+        private Name(final String pOperatorName) {
             operatorName = pOperatorName;
         }
 
