fe4753d9420b52213dd01e98d980090c79ac13b8
UPietruschka <ulf.pietruschka@etenso.com>
2013-09-05 15:23:51 +0200

    Test adaptations for the recently completed case-sensitivity support.

--- test/basic/src/main/resources/sql/CASE_SENSITIVE_TABLE.sql
+++ test/basic/src/main/resources/sql/CASE_SENSITIVE_TABLE.sql
@@ -6,7 +6,7 @@ CREATE TABLE "case_sensitive_table"
  "c2" VARCHAR2(64) NOT NULL,
  "version" NUMBER DEFAULT 1
 );
-ALTER TABLE "case_sensitive_table" ADD CONSTRAINT case_sensitive_table_pk PRIMARY KEY ("id");
+ALTER TABLE "case_sensitive_table" ADD CONSTRAINT "case_sensitive_table_pk" PRIMARY KEY ("id");
 
 DROP SEQUENCE "case_sensitive_table_seq";
 CREATE SEQUENCE "case_sensitive_table_seq";
